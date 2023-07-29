58c0792781a1f2c4e63be8d8cfc2e81284bee9e9
gofarrapeira brandao <gloria.andrade.brandao@gmail.com>
2023-07-29 21:53:01 +0000

    ComparandoObjetos

--- src/main/java/com/example/AppClassesErrado.java
+++ src/main/java/com/example/AppClassesErrado.java
@@ -2,11 +2,11 @@ package com.example;
 
 
 class cliente {
-    int AnoNascimento;
-    double Salario;
-    char Sexo;
+    private int AnoNascimento;
+    private double Salario;
+    private char Sexo;
 
-    double getSalario(){
+    public double getSalario(){
         return Salario;
     }
 
@@ -16,6 +16,27 @@ class cliente {
         else
         System.out.println("O valor do salario deve ser maior  que zero");
     }
+
+    public int getAnoNascimento(){
+        return AnoNascimento;
+    }
+    void setAnoNascimento(int AnoNascimento){
+        if (AnoNascimento>=1900)
+        this.AnoNascimento=AnoNascimento;
+        else
+        System.out.println("O ano de nascimento deve ser maoir que 1900");
+    }
+
+    public char getSexo(){
+        return Sexo;
+    }
+
+    void setSexo(char Sexo){
+        if (Sexo != 'M' &&  Sexo !='F')
+        System.out.println("So existem dois sexos: Feminino e Masculino");
+        else 
+        this.Sexo=Sexo;
+    }
 }
 
 public class AppClassesErrado {
@@ -23,23 +44,21 @@ public class AppClassesErrado {
     public static void main(String[] args) {
         System.out.println("Criando uma classe");
         cliente Cliente = new cliente();
-        Cliente.AnoNascimento=1983;
-        //Cliente.Salario= 12000;
+        Cliente.setAnoNascimento(1983);
         Cliente.setSalario(-2000);
-        Cliente.Sexo='F';
-        System.out.println("O ano de nascimento do cliente é:" + Cliente.AnoNascimento);
-        System.out.println("O salario do cliente é:" + Cliente.Salario);
-        System.out.println("O sexo do cliente é:" + Cliente.Sexo);
+        Cliente.setSexo('D');
+        System.out.println("O ano de nascimento do cliente é:" + Cliente.getAnoNascimento());
+        System.out.println("O salario do cliente é:" + Cliente.getSalario());
+        System.out.println("O sexo do cliente é:" + Cliente.getSexo());
 
         cliente Cliente2 = new cliente();
-        Cliente2.AnoNascimento=1968;
-        // Cliente2.Salario= 120000;
-         Cliente2.setSalario(140000);
-         Cliente2.Sexo='F';
+        Cliente2.setAnoNascimento(1968);
+        Cliente2.setSalario(140000);
+        Cliente2.setSexo('F');
 
-    System.out.println("O ano de nascimento do cliente é:" + Cliente2.AnoNascimento);
-    System.out.println("O salario do cliente é:" + Cliente2.Salario);
-    System.out.println("O sexo do cliente é:" + Cliente2.Sexo);
+    System.out.println("O ano de nascimento do cliente2 é:" + Cliente2.getAnoNascimento());
+    System.out.println("O salario do cliente2 é:" + Cliente2.getSalario());
+    System.out.println("O sexo do cliente2 é:" + Cliente2.getSexo());
  
     } 
 }
