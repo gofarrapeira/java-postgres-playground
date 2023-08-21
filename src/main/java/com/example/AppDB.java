e4aefb32502c2ffe32f15d84e358d317912a2739
gofarrapeira brandao <gloria.andrade.brandao@gmail.com>
2023-08-21 01:43:11 +0000

    AcessandoBD

--- src/main/java/com/example/AppDB.java
+++ src/main/java/com/example/AppDB.java
@@ -2,25 +2,30 @@ package com.example;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AppDB {
     public static void main(String[] args) {
     try {
-            Class.forName("org.postgresql.Driver");
-       var conn= DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "");
+            Class.forName("org.postgresql.Driver");}
+    catch (ClassNotFoundException e) {
+                System.err.println("Não conseguiu conectar ao banco de dados");
+             }
+    Statement statement =null;
+    try(var conn= DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "")){
         System.out.println("Conexao realidada com suceso");
 
-       var statement = conn.createStatement();
+       statement = conn.createStatement();
        var result = statement.executeQuery("select * from estado");
        while(result.next()){
-           System.out.printf("UF: %d  Nome: %S  UF: %s \n" , result.getInt("id"),result.getString("nome"),result.getString("UF"));
+           System.out.printf("Id: %d  Nome: %s  UF: %s \n" , result.getInt("id"),result.getString("nome"),result.getString("UF"));
         }
-    } catch (ClassNotFoundException e) {
-        System.err.println("Não conseguiu conectar ao banco de dados");
-     } catch (SQLException e) {
-        System.err.println("Nao foi possivel fazer conexao com o BD");
+    }  catch (SQLException e) {
+        if(statement==null){
+            System.err.println("Nao foi possivel fazer conexao com o BD");}
+        else{
+             System.err.println("Erro na consulta ao banco de dados");}
     }
       
-}
-}
-
+    }}
+    
